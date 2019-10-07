d1b8197ea3a63716e5e7c27ef71d928b789f8924
medingr <32764049+medingr@users.noreply.github.com>
2019-10-07 13:35:21 +0800

    Add files via upload

--- main/main.js
+++ main/main.js
@@ -1,4 +1,18 @@
 module.exports = function main() {
     console.log("Debug Info");
     return 'Hello World!';
+
+    function printInventory (inputs){
+    var finalResult = [];
+    var sameBarcode = [...new Set(inputs.map(item => Barcode.Group))];
+
+    var count = dataset.inputs(function(n, val) {
+    return n + (val === sameBarcode);
+    }, 0);
+
+    const sameBarcode = inputs.map(x => x.Barcode);
+    sameBarcode.unique();
+
+    }
+
 };
