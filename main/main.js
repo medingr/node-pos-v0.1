7d004073ea1d8927d23e93aa275cf7ebbcf29e5b
medingr <32764049+medingr@users.noreply.github.com>
2019-10-08 14:48:29 +0800

    refactored Main

--- main/main.js
+++ main/main.js
@@ -1,55 +1,54 @@
 module.exports = function main(inputs) {
     console.log("Debug Info");
-   
 
-    // var productList = populateProductList(inputs); 
     var printReceipt = printInventory (inputs);
+    return printReceipt;
 
-    function printInventory (inputs){
-       var productList = populateProductList(inputs); 
-      let ReceiptToPrint = '***<store earning no money>Receipt ***\n' ;
-      productList.forEach(product => {
-        let nameOfProduct = 'Name: ' + product.Name;
-        let quantityOfProduct = ', Quantity: ' + product.Count;
-        let unitPrice = ', Unit price: ' + product.Price;
-        let productSubTotal = ', Subtotal: ' + product.Subtotal;
-        let total = total + productSubTotal;
-        let finalDetails = nameOfProduct + quantityOfProduct + unitPrice + productSubTotal + total +  '/\n';
-        ReceiptToPrint = ReceiptToPrint.concat(finalDetails);
+};
+
+function printInventory (inputs){
+    var productList = populateProductList(inputs); 
+    let ReceiptToPrint = '***<store earning no money>Receipt ***\n' ;
+    let total = 0;
+    
+    productList.forEach(product => {
+        total +=  product.Subtotal;
+        let unit = getUnitOfProduct(product.Unit);
+        ReceiptToPrint+=`Name: ${product.Name}, Quantity: ${product.Count}${unit}` +
+            `, Unit price: ${product.Price.toFixed(2)} (yuan), Subtotal: ${product.Subtotal.toFixed(2)} (yuan)`+ `\n` ;
     });
 
-    var adults_sum = 0, children_sum = 0;
+    ReceiptToPrint += '----------------------\n' +
+                     `Total: ${total.toFixed(2)} (yuan) \n` +
+                    '**********************\n';
 
-array.forEach(function(obj){
-    adults_sum += obj["adults"];
-    children_sum += obj["children"];
-});
+    return ReceiptToPrint;
+};
 
-        return ReceiptToPrint;
-    };
+function getUnitOfProduct (product) {
+    return product == "bottle" ? " bottles" : "";
+}
 
-    function populateProductList(inputs){
-        let uniqueBarCodes = getUniqueBarCodes(inputs);
-        
-        let productList=[];
-        for(let uniqueBarCode of uniqueBarCodes){
-            let product = inputs
-                .filter(input=>input.Barcode === uniqueBarCode)
-                [0];
-            let productCount = inputs
-            .filter(input=> input.Barcode === uniqueBarCode).length;
-            let subtotal = productCount * product.Price;
-            product.Subtotal= subtotal;
-            product.Count= productCount;
-            productList.push(product);
-        }
-
-        return productList;
-    };
+function populateProductList(inputs){
+    let uniqueBarCodes = getUniqueBarCodes(inputs);
     
-    function getUniqueBarCodes (inputs){
-        var  listOfUniqueBarCodesandPrice = [...new Set( inputs.map(x => x.Barcode))];
-        return listOfUniqueBarCodesandPrice;
-    };
+    let productList=[];
+    for(let uniqueBarCode of uniqueBarCodes){
+        let product = inputs
+            .filter(input=>input.Barcode === uniqueBarCode)
+            [0];
+        let productCount = inputs
+            .filter(input=> input.Barcode === uniqueBarCode).length;
+        let subtotal = productCount * product.Price;
+        product.Subtotal= subtotal;
+        product.Count= productCount;
+        productList.push(product);
+    }
 
+    return productList;
+};
+
+function getUniqueBarCodes (inputs){
+    var  listOfUniqueBarCodesandPrice = [...new Set( inputs.map(x => x.Barcode))];
+    return listOfUniqueBarCodesandPrice;
 };
